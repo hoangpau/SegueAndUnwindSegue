bd4fb6af9a5d3bb9846149f3940680adec2bcb33
ThangTruong <thangtm6@fsoft.com.vn>
2017-03-27 10:13:54 +0700

    Unwind segue VS Segue

--- SegueAndUnwindSegue/ViewController.swift
+++ SegueAndUnwindSegue/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class VC1: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func unwindFromVC2ToVC1(for unwindSegue: UIStoryboardSegue) {
+        print("Unwind")
+    }
 
+}
+
+
+class VC2: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
 }
 
